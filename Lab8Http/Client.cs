c0fb5637bb7419e5273439e256b39b5bb4fbda25
Silkou Maxim <maxim.silkou.2002@gmail.com>
2022-05-12 15:49:13 +0300

    Add project files.

---
+++ Lab8Http/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Lab8Http
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write Uri-address: ");
+            string uriAddress = Console.ReadLine();
+            HttpClient client = new HttpClient();
+
+            Uri uri = new Uri(uriAddress);
+
+            string result = client.GetStringAsync(uri).Result;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Server
+{
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            HttpListener listener = new HttpListener();
+
+            // установка адресов прослушки
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Waiting connection...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContextAsync().Result;
+                
+                //запрос
+                HttpListenerRequest request = context.Request;
+                
+                //ответ
+                HttpListenerResponse response = context.Response;
+
+                Console.WriteLine("Status code: " + response.StatusCode);
+                Console.WriteLine("Headers: " + response.Headers);
+
+                string responseString = "<html><head><meta charset='utf8'></head><body>Привет мир!</body></html>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                // закрываем поток
+                output.Close();
+
+                // останавливаем прослушивание подключений
+                Console.WriteLine("End...");
+            }
+        }
+    }
+}
